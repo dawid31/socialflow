8e5102bd75c22e79e6ff421183d40071e67b3f1c
dawid31 <dawidkedzierski04@gmail.com>
2022-09-23 16:17:30 +0200

    used django signals to create profile for each new registered user automatically added likes counter to posts

--- DjangoBlogApp/models.py
+++ DjangoBlogApp/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class Post(models.Model):
@@ -21,8 +23,16 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
     biogram = models.TextField(max_length=512)
     avatar = models.ImageField(null=True, blank=True, default='default.png')
-    
-    
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+        
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs): 
+        instance.profile.save()
+
     def __str__(self):
         return f"{self.user.username} profile"
 
