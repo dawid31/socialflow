dead55b3593299b959a4afea69537eb8e66e872f
dawid31 <dawidkedzierski04@gmail.com>
2023-02-26 22:50:04 +0100

    settings.py update

--- DjangoBlogProject/settings.py
+++ DjangoBlogProject/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY") #this line replaces previous secret key to make it private
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['socialflow-production-92f2.up.railway.app']
 
 
 # Application definition
